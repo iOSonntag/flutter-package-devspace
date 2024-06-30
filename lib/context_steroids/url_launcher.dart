6f983c297442c2310372159b6ec5f7c9e3730c3b
iOSonntag <dev@iosonntag.com>
2024-06-30 22:15:08 +0200

    feat: add url launcher feature

--- lib/context_steroids/url_launcher.dart
+++ lib/context_steroids/url_launcher.dart
@@ -5,10 +5,16 @@ part of devspace;
 
 extension ExtensionContextUrlLauncher on BuildContext
 {
-  Future<void> launchUrl(String url) async
+  Future<void> launchUrl(String url, {
+    bool webOpenInNewTap = true
+  }) async
   {
     final Uri uri = Uri.parse(url);
 
-    if (!await p_url_launcher.launchUrl(uri)) throw 'Could not launch $uri';
+    final launched = await p_url_launcher.launchUrl(uri,
+      webOnlyWindowName: webOpenInNewTap ? '_blank' : '_self'
+    );
+
+    if (!launched) throw 'Could not launch $uri';
   }
 }
