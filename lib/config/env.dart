b3840d48020655000fe16fdceaa6fd2b51c0e54f
iOSonntag <dev@iosonntag.com>
2024-07-23 23:39:50 +0200

    feat: add platform chooser

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -28,7 +28,12 @@ abstract class Env {
   static bool get isLocal => environment == kEnvironment.local;
 
 
-  static String chooseString({required String prod, required String stage, required String dev, required String local}) 
+  static String chooseString({
+    required String prod, 
+    required String stage, 
+    required String dev, 
+    required String local
+  }) 
   {
     switch (environment)
     {
@@ -43,6 +48,74 @@ abstract class Env {
       default:
         return dev;
     }
+  }
+
+  static String choosePlatformString({
+    required bool throwOnNotSupplied,
+    String? ios, 
+    String? android,
+    String? web,
+    String? macos,
+    String? windows,
+    String? linux,
+  }) 
+  {
+    if (kIsWeb)
+    {
+      if (web == null && throwOnNotSupplied)
+      {
+        throw Exception('Web platform string not supplied');
+      }
+      return web ?? '';
+    }
+    
+    if (Platform.isIOS)
+    {
+      if (ios == null && throwOnNotSupplied)
+      {
+        throw Exception('iOS platform string not supplied');
+      }
+      return ios ?? '';
+    }
+    
+    if (Platform.isAndroid)
+    {
+      if (android == null && throwOnNotSupplied)
+      {
+        throw Exception('Android platform string not supplied');
+      }
+      return android ?? '';
+    }
+    
+    if (Platform.isMacOS)
+    {
+      if (macos == null && throwOnNotSupplied)
+      {
+        throw Exception('macOS platform string not supplied');
+      }
+      return macos ?? '';
+    }
+    
+    if (Platform.isWindows)
+    {
+      if (windows == null && throwOnNotSupplied)
+      {
+        throw Exception('Windows platform string not supplied');
+      }
+      return windows ?? '';
+    }
+    
+    if (Platform.isLinux)
+    {
+      if (linux == null && throwOnNotSupplied)
+      {
+        throw Exception('Linux platform string not supplied');
+      }
+      return linux ?? '';
+    }
+    
+
+    throw Exception('Platform not supported');
   }
 
 }
